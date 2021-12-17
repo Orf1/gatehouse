cf8c93faf05bd2e4eefef6e9733546eef47ffba7
Orf1 <orfeas126@gmail.com>
2021-12-16 21:06:03 -0800

    Update Application.kt

--- src/main/kotlin/orf1/dev/Application.kt
+++ src/main/kotlin/orf1/dev/Application.kt
@@ -7,5 +7,5 @@ import orf1.dev.plugins.configureSecurity
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
         configureSecurity()
-    }.start(wait = true)
+    }.start(true)
 }
